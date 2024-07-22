ae6d2e1ae10970ffa763ea00edc38d6ed0a9bcec
Ezgi Sümer Günaydın <ezgisumer00@gmail.com>
2024-07-22 16:25:29 +0300

    creating project

---
+++ ShoppingApp/ShoppingApp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  ShoppingApp
+//
+//  Created by Ezgi Sümer Günaydın on 22.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+

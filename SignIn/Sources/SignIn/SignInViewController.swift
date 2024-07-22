5160e588b63c31f744615c424ee4b9180d71dcb1
Ezgi Sümer Günaydın <ezgisumer00@gmail.com>
2024-07-22 18:47:42 +0300

    creating signIn screen and preparing its UI, preparing for google auth

--- SignIn/Sources/SignIn/SignInViewController.swift
+++ SignIn/Sources/SignIn/SignInViewController.swift
@@ -10,24 +10,53 @@ import Firebase
 import FirebaseCore
 import FirebaseAuth
 import GoogleSignIn
+import GoogleSignInSwift
 
-class SignInViewController: UIViewController {
+public class SignInViewController: UIViewController {
 
-    override func viewDidLoad() {
+    @IBOutlet weak var signInWithGoogle: GIDSignInButton!
+    public override func viewDidLoad() {
         super.viewDidLoad()
+        setupGoogleAuth()
+    }
 
-        // Do any additional setup after loading the view.
+    // MARK: - Module init
+    public init() {
+        super.init(nibName: String(describing: Self.self), bundle: Bundle.module)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+//MARK: - Setups
+extension SignInViewController {
+    func setupGoogleAuth() {
+        let tapGesture = UITapGestureRecognizer(target: self, action:  #selector(googleSignInTapped))
+        signInWithGoogle.addGestureRecognizer(tapGesture)
     }
-    */
-
+    
+    @objc func googleSignInTapped() {
+        guard let clientID = FirebaseApp.app()?.options.clientID else { return }
+        
+        // Create Google Sign In configuration object.
+        let config = GIDConfiguration(clientID: clientID)
+        GIDSignIn.sharedInstance.configuration = config
+        
+        // Start the sign in flow!
+        GIDSignIn.sharedInstance.signIn(withPresenting: self) { result, error in
+            guard error == nil,
+                  let user = result?.user,
+                  let idToken = user.idToken?.tokenString
+            else { return }
+            let credential = GoogleAuthProvider.credential(withIDToken: idToken, accessToken: user.accessToken.tokenString)
+            Auth.auth().signIn(with: credential) { result, error in
+                guard let _ = result, nil == nil else { return }
+            }
+          
+        }
+    }
 }
---
+++ SignIn/Sources/SignIn/SignInViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SignInViewModel.swift
+//
+//
+//  Created by Ezgi Sümer Günaydın on 22.07.2024.
+//
+
+import Foundation

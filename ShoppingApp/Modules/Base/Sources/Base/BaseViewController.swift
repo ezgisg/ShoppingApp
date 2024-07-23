6b00dc01550716872196faab3ed7bf8ffbd94d29
Ezgi Sümer Günaydın <ezgisumer00@gmail.com>
2024-07-23 21:37:09 +0300

    extend keyboard observer functions

--- ShoppingApp/Modules/Base/Sources/Base/BaseViewController.swift
+++ ShoppingApp/Modules/Base/Sources/Base/BaseViewController.swift
@@ -18,6 +18,7 @@ public protocol BaseViewControllerProtocol: AnyObject {
 // MARK: - BaseViewController
 open class BaseViewController: UIViewController, LoadingShowable {
     private var tapGesture: UITapGestureRecognizer!
+    public var activeTextField: UITextField?
     
     open override func viewDidLoad() {
         super.viewDidLoad()
@@ -48,25 +49,47 @@ extension BaseViewController: BaseViewControllerProtocol {
 
 //MARK: Keyboard operations
 ///When keyboard is active then other tap properties disabled and when tap anywhere else but keyboard then keyboard hide
+///Checking whether the selected textfield remains under the keyboard. If it is under keyboard then shifting the view. For this activeTextField must set
 public extension BaseViewController {
-    final func setupKeyboardObservers() {
-          NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
-          NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
-      }
-    
-      @objc final func keyboardWillShow() {
-          if tapGesture == nil {
-              tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
-          }
-          view.addGestureRecognizer(tapGesture)
-      }
-      
-      @objc final func keyboardWillHide() {
-          if tapGesture != nil {
-              view.removeGestureRecognizer(tapGesture)
-          }
-      }
-      @objc final func dismissKeyboard() {
-          view.endEditing(true)
-      }
+    final func setupKeyboardObservers(activeTextField: UITextField? = nil) {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+        self.activeTextField = activeTextField
+    }
+
+    @objc final func keyboardWillShow(notification: NSNotification) {
+        if tapGesture == nil {
+            tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        }
+        view.addGestureRecognizer(tapGesture)
+        
+        ///To scroll the view up
+        guard let keyboardFrame = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue,
+              let activeTextField = activeTextField else {
+            return
+        }
+
+        let keyboardTop = view.frame.height - keyboardFrame.height
+        let textFieldBottom = activeTextField.convert(activeTextField.bounds, to: view).maxY
+
+        if textFieldBottom + 40 > keyboardTop {
+            UIView.animate(withDuration: 0.3) { [weak self] in
+                guard let self else { return }
+                view.frame.origin.y = keyboardTop - textFieldBottom - 40
+            }
+        }
+    }
+
+    @objc final func keyboardWillHide(notification: NSNotification) {
+        if tapGesture != nil {
+            view.removeGestureRecognizer(tapGesture)
+        }
+        ///To return the view to its original place
+        view.frame.origin.y = 0
+    }
+
+    @objc final func dismissKeyboard() {
+        view.endEditing(true)
+    }
 }
--- ShoppingApp/Modules/SignIn/Sources/SignIn/SignIn/SignInViewController.swift
+++ ShoppingApp/Modules/SignIn/Sources/SignIn/SignIn/SignInViewController.swift
@@ -40,16 +40,13 @@ public class SignInViewController: BaseViewController {
     //MARK: Module Components
     var viewModel = SignInViewModel()
     
-    
-    var activeTextField: UITextField?
-    
     //MARK: - Life Cycles
     public override func viewDidLoad() {
         super.viewDidLoad()
         setupGoogleAuth()
         setupUI()
         setupKeyboardObservers()
-        setupKeyboardObservers2()
+ 
     }
     
     // MARK: - Module init
@@ -239,30 +236,3 @@ extension SignInViewController: UITextFieldDelegate {
 }
 
 
-extension SignInViewController {
-    private func setupKeyboardObservers2() {
-          NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow2), name: UIResponder.keyboardWillShowNotification, object: nil)
-          NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide2), name: UIResponder.keyboardWillHideNotification, object: nil)
-      }
-      
-      @objc func keyboardWillShow2(notification: NSNotification) {
-          guard let keyboardFrame = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue,
-                let activeTextField = activeTextField else {
-              return
-          }
-
-          let keyboardTop = view.frame.height - keyboardFrame.height
-          let textFieldBottom = activeTextField.convert(activeTextField.bounds, to: view).maxY
-
-          if textFieldBottom + 40 > keyboardTop {
-              UIView.animate(withDuration: 0.3) {  [weak self] in
-                  guard let self else { return }
-                  view.frame.origin.y = keyboardTop - textFieldBottom - 40
-              }
-          }
-      }
-
-      @objc func keyboardWillHide2(notification: NSNotification) {
-          view.frame.origin.y = 0
-      }
-}
